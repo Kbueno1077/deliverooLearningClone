af5169d63b9b8f3b184ef2a39c2cbab487f703fb
Kbueno1077 <kbueno1077@gmail.com>
2024-05-03 17:46:48 -0400

    Horizontal Scrolls

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import Categories from "@/components/Categories/Categories";
+import Restaurants from "@/components/Restaurants/Restaurants";
+import Colors from "@/constants/Colors";
+import { StyleSheet, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
     return (
-        <View>
-            <Text style={styles.title}>Page</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+                <Categories />
+
+                <Text style={styles.header}>Top picks near you </Text>
+                <Restaurants />
+
+                <Text style={styles.header}>Offers near you </Text>
+                <Restaurants />
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
+    container: { top: 50, backgroundColor: Colors.lightGrey },
+    header: {
+        fontSize: 18,
         fontWeight: "bold",
+        marginTop: 16,
+        marginBottom: 8,
+        paddingHorizontal: 16,
     },
 });
